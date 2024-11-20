dd36784afe97500c915af2a828fc765919f6a39a
Lyon <wuliyanglyon@gmail.com>
2024-11-20 10:04:44 +0800

    url_test.py

--- test/url_test.py
+++ test/url_test.py
@@ -1,16 +1,53 @@
 from datetime import datetime
 from wsgiref.handlers import format_date_time
 from time import mktime
+from urllib.parse import urlencode
+import hmac
+import hashlib
+import base64
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-now = datetime.now()
-date1 = now.timetuple()
-date2 = mktime(date1)
-date = format_date_time(mktime(now.timetuple()))
-logger.info(now)
-logger.info(date1)
-logger.info(date2)
-logger.info(date)
+API_SECRET = 'NjI1OWNkOGQwZTliNTIxYzg3ZjE2MzIy'
+API_KEY = '38044e44441372ce9ca4d645757fd897'
+
+def create_url(APISecret, APIKey):
+        url = "wss://tts-api.xfyun.cn/v2/tts"
+        # 生成RFC1123格式的时间戳
+        now = datetime.now()
+        date = format_date_time(mktime(now.timetuple()))
+
+        # 拼接字符串
+        signature_origin = "host: " + "ws-api.xfyun.cn" + "\n"
+        signature_origin += "date: " + date + "\n"
+        signature_origin += "GET " + "/v2/tts " + "HTTP/1.1"
+        # 进行hmac-sha256进行加密
+        signature_sha = hmac.new(
+            APISecret.encode("utf-8"),
+            signature_origin.encode("utf-8"),
+            digestmod=hashlib.sha256,
+        ).digest()
+        signature_sha = base64.b64encode(signature_sha).decode(encoding="utf-8")
+
+        authorization_origin = (
+            'api_key="%s", algorithm="%s", headers="%s", signature="%s"'
+            % (APIKey, "hmac-sha256", "host date request-line", signature_sha)
+        )
+        authorization = base64.b64encode(authorization_origin.encode("utf-8")).decode(
+            encoding="utf-8"
+        )
+        # 将请求的鉴权参数组合为字典
+        v = {"authorization": authorization, "date": date, "host": "ws-api.xfyun.cn"}
+        # 拼接鉴权参数，生成url
+        url = url + "?" + urlencode(v)
+        # print("date: ",date)
+        # print("v: ",v)
+        # 此处打印出建立连接时候的url,参考本demo的时候可取消上方打印的注释，比对相同参数时生成的url与自己代码生成的url是否一致
+        print('websocket url :', url)
+        return url
+
+
+if __name__ == '__main__':
+        create_url(API_SECRET, API_KEY)
